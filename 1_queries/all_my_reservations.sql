fcdaad234ccfa20a5966e56256725cbb330fa5e4
Abhiram <abhiram.satha@gmail.com>
2022-01-21 01:11:06 +0000

    Adding zip file provided by lighthouse labs

--- 1_queries/all_my_reservations.sql
+++ 1_queries/all_my_reservations.sql
@@ -3,6 +3,7 @@ FROM reservations
 JOIN properties ON reservations.property_id = properties.id
 JOIN property_reviews ON properties.id = property_reviews.property_id
 WHERE reservations.guest_id = 1
-GROUP BY properties.id, reservations.id
+GROUP BY reservations.id, properties.id
 ORDER BY reservations.start_date
 LIMIT 10;
+
